eb1cfeef6c1be9068be47bc7c674e540d2625809
Ennuilw <104221612+Ennuilw@users.noreply.github.com>
2022-08-14 08:33:38 +0900

    Update setting.py

--- setting.py
+++ setting.py
@@ -1,3 +1,13 @@
 token = ""
-Dev = #Discord user id
-DEV = #Discord usre id
+y_token= ""
+Dev = 
+DEV_ = 
+spotify_client_id = ""
+spotify_client_secret=""
+funshi_invite="https://discord.gg/"
+Yufu_yt = ""
+sp_logo_defa = "https://media.discordapp.net/attachments/973063112548814878/1007736488265523280/Spotify_logo_without_text.svg.png?width=671&height=671"
+sp_logo_wh = "https://media.discordapp.net/attachments/973063112548814878/1007736897570873384/Spotify_Icon_RGB_White.png?width=671&height=671"
+fav= 0x6dc1c1
+S_color=0x1db954
+img_path = 'image.png'
